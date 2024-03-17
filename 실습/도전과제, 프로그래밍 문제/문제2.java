ccfc194ba7c9ce661f0db648d153b93f3ed03202
kysshl7 <163219038+kysshl7@users.noreply.github.com>
2024-03-18 00:05:36 +0900

    Add files via upload

---
+++ 실습/도전과제, 프로그래밍 문제/문제2.java
@@ -0,0 +1,16 @@
+package 프로그래밍문제;
+
+import java.util.Scanner;
+
+public class 문제2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		System.out.print("정수를 입력하세요 : ");
+		int a = in.nextInt();
+		System.out.printf("%d의 제곱은 %d", a, a * a);
+
+	}
+
+}
