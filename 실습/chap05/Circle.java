5d2ba2dad5dc88f70e8a73d09c40f43f3ead187a
kysshl7 <163219038+kysshl7@users.noreply.github.com>
2024-04-03 12:25:39 +0900

    Add files via upload

---
+++ 실습/chap05/Circle.java
@@ -0,0 +1,18 @@
+package chap05;
+
+public class Circle {
+	double radius;
+
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	public double getRadius() {
+		return radius;
+	}
+
+	double findArea() {
+		return 3.14 * radius * radius;
+	}
+
+}
