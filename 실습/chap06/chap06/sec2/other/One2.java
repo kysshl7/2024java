304df81b63bcefc0e55a15396daca26f79cb9ac7
kysshl7 <163219038+kysshl7@users.noreply.github.com>
2024-04-18 13:04:45 +0900

    Add files via upload

---
+++ 실습/chap06/chap06/sec2/other/One2.java
@@ -0,0 +1,11 @@
+package chap06.sec2.other;
+
+import chap06.sec02.One;
+
+public class One2 extends One {
+	void print() {
+		System.out.println(child);
+		System.out.println(anybody);
+	}
+
+}
---
+++ 실습/chap06/chap06/sec2/other/Three.java
@@ -0,0 +1,11 @@
+package chap06.sec2.other;
+
+import chap06.sec02.One;
+
+public class Three {
+	void print() {
+		One o = new One();
+		System.out.println(o.anybody);
+	}
+
+}
