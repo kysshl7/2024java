e05e1379a4dccc0bf71a71f5318f671b1dae704d
kysshl7 <163219038+kysshl7@users.noreply.github.com>
2024-06-20 04:26:26 +0900

    Add files via upload

---
+++ 실습/chap12/프로그래밍문제/문제2.java
@@ -0,0 +1,15 @@
+package 프로그래밍문제;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 문제2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str=br.readLine();
+
+        System.out.println(str);
+    }
+}
+
---
+++ 실습/chap12/프로그래밍문제/문제6.java
@@ -0,0 +1,10 @@
+package 프로그래밍문제;
+
+public class 문제6 {
+    public static void main(String[] args) {
+        String[] animals = {"ant", "bat", "cat", "dog"};
+
+        for (String s : animals)
+            System.out.println(s.charAt(0) + " : " + s);
+    }
+}
